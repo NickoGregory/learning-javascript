1adaf6aa4cf0d1bcf804ca4d8a8019dcbad937ac
NickoGregory <nickogregory@gmail.com>
2024-12-27 11:48:25 -0500

    update on js lesson

--- java.js
+++ java.js
@@ -5,5 +5,69 @@ An algorithm is a step-by-step procedure or formula for solving a problem
  /**A data structure is a way of organizing, managing, and storing data to
   *  enable efficient access and modification.  */
   
- // In the lesson we will learn the basics of javascript and build out a pyramid generator as we following along with freecodecamp lessons.
- 
+ // In the lesson we will learn the basics of javascript and build out a pyramid 
+ // generator as we following along with freecodecamp lessons.
+
+//  The first lesson to build a pyramid generator is to learn about variables.
+// this is often done using let keyword,variable nameing follows a specific rule,
+/** names can include letters, numbers, dollar signs, and underscores, but cannot 
+ * contain spaces and must not begin with a number.
+
+Use the let keyword to declare a variable called character.
+
+Note: It is common practice to end statements in JavaScript with a semicolon. ; */
+
+let character;
+
+// lesson 2.
+
+// this lesson teaches how to assign a value to a variable.
+// by using the assignment operator you can assign a value,this is call
+// intialization 
+// i added the string value of "hello" to the character variable.
+let character = "hello";
+
+// lesson 3.
+
+//  this lesson teaches you about the string value
+// this specific lesson states that there is 2 ways to write string its by using sigle or double quotes.
+// i replaced the double qoutes for the value of the character variable from double to single.
+let character = 'hello'
+
+// lesson 4.
+
+// This lesson teaches you about the console.log, this is method with the function to output 
+// the value of a variable.
+// i added the console.log method to print the value of character variable to the console log.
+let character = 'Hello';
+console.log(character);
+
+// lesson 5.
+
+// this lesson teaces you how to reasign/replace the value of a variable.
+// I change the value of the variable from hello to world.
+let character = 'Hello';
+character= "World"
+console.log(character);
+
+// lesson 6.
+
+// This lesson teaches about you how to name varuables using camel case, 
+// this means that each word in your name should start with a capital letter. 
+// I added a new variable called secondCharacter; without intializing it.
+// this is called uninitialized.
+let character = 'Hello';
+character= "World"
+console.log(character);
+let secondCharacter;
+
+// lesson 7.
+
+// this lesson teaches you how to assign a value to a unitialized variable.
+// i added a value to a unitializled variable.
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test"
+console.log(secondCharacter);
