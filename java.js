2a60f74a395b394b69699093b98ce98ec59c967b
NickoGregory <nickogregory@gmail.com>
2024-12-27 09:28:44 -0500

    Add some notes in javascript

---
+++ java.js
@@ -0,0 +1,9 @@
+/**What is an Algorithm?
+An algorithm is a step-by-step procedure or formula for solving a problem
+ or performing a task. In JavaScript,  */
+
+ /**A data structure is a way of organizing, managing, and storing data to
+  *  enable efficient access and modification.  */
+  
+ // In the lesson we will learn the basics of javascript and build out a pyramid generator as we following along with freecodecamp lessons.
+ 
